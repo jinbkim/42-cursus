18d3e5226b867eeb1f0952ddc6fe36e39ac951e0
jinbkim <jinbkim@jinbkimui-MacBookAir.local>
2021-02-17 15:52:31 +0900

    cpp04

--- cpp_module/cpp_04/ex00/main.cpp
+++ cpp_module/cpp_04/ex00/main.cpp
@@ -1,7 +1,4 @@
 // 주어진 메인문으로 원하는 출력값을 출력되도록 클래스만들기
-// Sorcerer 클래스 만들기
-// Victim 클래스 만들기
-// Peon 클래스 만들기
 
 // 가상함수에 대해 알아가는 문제
 
@@ -9,6 +6,8 @@
 // Victim에서 getPolymorphed를 가상함수로 만들어주면서 Victim을 상속받은 Peon에서 getPolymorphed를 재정의함
 
 // 클래스에서 가상 함수를 정의할 때 소멸자도 가상함수로 만들어야 함
+
+// Sorcerer객체와 Victim객체가 cout으로 잘 출력되기위해 <<연산자 오버로딩을 해줌
 
 // Sorcerer클래스는 매개변수가 없으면 객체를 못만들게 만들어야함
 
@@ -25,7 +24,7 @@ int main()
 	robert.polymorph(jim);
 	robert.polymorph(joe);  // Victim에서 polymorph를 가상함수로 만들었기 때문에 Peon에서 재정의한 함수가 호출됨
 
-	// Sorcerer	a;  // 인자없이 만들수 없음
+	Sorcerer	a;  // 인자없이 만들수 없음
 
 	return 0;
 }
