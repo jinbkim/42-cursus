1890d2e5b6a3c182cbd4efe20df4a9d36fd90d90
Jinbum Kim <jinbkim@c4r5s6.42seoul.kr>
2021-02-24 18:06:03 +0900

    cpp06

--- cpp_module/cpp_06/ex01/main.cpp
+++ cpp_module/cpp_06/ex01/main.cpp
@@ -1,5 +1,5 @@
-// 8크기의 랜덤 문자열(알파벳or숫자) + 랜덤 int형 변수+ 8크기의 랜덤 문자열(알파벳or숫자)
-// 8+4+8 데이터로 직렬화하는 serialize함수 만들기
+// 8글자인 랜덤 문자열(알파벳or숫자) + 랜덤 int형 변수+ 8글자인 랜덤 문자열(알파벳or숫자)
+// 24+4+24 데이터로 직렬화하는 serialize함수 만들기
 
 // 직렬화한 데이터를 역직렬화해서 데이터를 뽑아내는 deserialize 함수만들기
 
@@ -15,38 +15,36 @@
 
 struct Data
 {
-    char	*s1;
+    std::string s1;
     int		n;
-	char	*s2;
+	std::string s2;
 };
 
-// 8크기의 랜덤 문자열(알파벳or숫자) + 랜덤 int형 변수+ 8크기의 랜덤 문자열(알파벳or숫자)
+// 8글자인 랜덤 문자열(알파벳or숫자) + 랜덤 int형 변수+ 8글자인 랜덤 문자열(알파벳or숫자)
 // 데이터를 직렬화함
 void    *serialize(void)
 {
+    std::string s1 = "";
+    std::string s2 = "";
     char	*ptr;
-    char	s1[8];
-    char	s2[8];
-    int		integer;
     char	alphanum[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    int		integer;
 
     srand(clock());  // 난수를 위해 사용하는 함수
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 8; i++)
     {
-        s1[i] = alphanum[rand() % 35];  // 8크기의 랜덤 문자열(알파벳or숫자)
-        s2[i] = alphanum[rand() % 35];  // 8크기의 랜덤 문자열(알파벳or숫자)
+        s1 += alphanum[rand() % 35];  // 8글자의 랜덤 문자열(알파벳or숫자)
+        s2 += alphanum[rand() % 35];  // 8글자의 랜덤 문자열(알파벳or숫자)
     }
-	s1[7] = '\0';
-	s2[7] = '\0';
     integer = rand();  // 랜덤 int형변수를 반환하는 함수
-    ptr = new char[sizeof(s1) + sizeof(int) + sizeof(s2)];  // 문자열2개의 크기와 int형변수의 크기만큼 메모리 할당
+    ptr = new char[sizeof(s1) + sizeof(integer) + sizeof(s2)];  // 문자열2개의 크기와 int형변수의 크기만큼 메모리 할당
 	std::cout<<"s1 : "<<s1<<'\n';
 	std::cout<<"integer : "<<integer<<'\n';
 	std::cout<<"s2 : "<<s2<<'\n';
 	
-    memcpy(ptr, s1, 8);  // ptr = s1	
-    memcpy(ptr + 8, &integer, 4);  // ptr = s1 + integer 
-    memcpy(ptr + 12, s2, 8);  // ptr = s1 + integer + s2
+    memcpy(ptr, &s1, 24);  // ptr = s1	
+    memcpy(ptr + 24, &integer, 4);  // ptr = s1 + integer 
+    memcpy(ptr + 28, &s2, 24);  // ptr = s1 + integer + s2
     return (ptr);
 }
 
@@ -56,11 +54,12 @@ Data    *deserialize(void *raw)
     Data *dd = new Data;
 
 	// reinterpret_cast<char*>(raw) 해도됨
+    // dd->s1 = static_cast<char*>(raw);  // raw 가리키기
     dd->s1 = static_cast<char*>(raw);  // raw 가리키기
 	// 8byte만큼 이동하기위해 먼저 static_cast<char*>(raw)
-	// dd->n = *static_cast<int*>(static_cast<char*>(raw) + 8); // 에러
-    dd->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 8);  // raw+8이후로 int 크기만큼 데이터 담기
-    dd->s2 = static_cast<char*>(raw) + 12;  // raw+12 가리키기 
+	// dd->n = *static_cast<int*>(static_cast<char*>(raw) + 24); // 에러
+    dd->n = *reinterpret_cast<int*>(static_cast<char*>(raw) + 24);  // raw+24 이후로 int 크기만큼 데이터 담기
+    dd->s2 = static_cast<char*>(raw) + 28;  // raw+28 가리키기 
 
     return (dd);
 }
